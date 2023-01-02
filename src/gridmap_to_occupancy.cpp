e682992cf22ffee52f5d862e99c5c4ec1093e431
Vsevolod Hulchuk <vsevolod.hulchuk@gmail.com>
2023-01-02 18:04:51 +0100

    add traversability map

--- src/gridmap_to_occupancy.cpp
+++ src/gridmap_to_occupancy.cpp
@@ -12,7 +12,9 @@
 static std::string topic = "/elevation_mapping/elevation_map";
 
 ros::Publisher m_mapPub;
-// using namespace grid_map;
+ros::Publisher travPub;
+
+
 
 void gridmap_callback(const grid_map_msgs::GridMap& msg){
   std::cout << "Got a Gridmap!" << std::endl;
@@ -21,7 +23,39 @@ void gridmap_callback(const grid_map_msgs::GridMap& msg){
   grid_map::GridMapRosConverter::fromMessage(msg, gridMap);
   grid_map::GridMapRosConverter::toOccupancyGrid(gridMap, "elevation", 0.0, 1.0, occupancyGrid);
   m_mapPub.publish(occupancyGrid);
+  Eigen::MatrixXf m = gridMap.get("elevation");
+  Eigen::MatrixXf m1(m);
+  int a = 0;
+
+  int nCells = m.cols()*m.rows();
+  for (int i = 1; i < m.cols() - 1 ; i++)
+  {
+    for (int j = 1; j < m.rows() - 1; j++)
+    {
+      a++;
+      float v = m.coeff(i, j);
+      size_t index = m.cols()*m.rows() - (j*m.rows() + i) - 1;
+      if (!isnan(v)){
+        float v_up = m.coeff(i+1, j);
+        float v_right = m.coeff(i, j+1);
+        float diff1 = std::fabs(v - v_up);
+        float diff2 = std::fabs(v - v_right);
+        float slope = std::sqrt(diff1*diff1 + diff2*diff2);
+        int value = 0;
+        if (slope > 0.1){
+          value = 100;
+        }
+        occupancyGrid.data[index] = value;
+      }
+      else{
+        occupancyGrid.data[index] = -1;
+      }
+    }
+  }
+  travPub.publish(occupancyGrid);
 }
+
+
 
 int main(int argc, char **argv){
 
@@ -34,6 +68,7 @@ int main(int argc, char **argv){
   std::cout << "Gridmap Topic: " << topic << std::endl;
 
   m_mapPub = nh.advertise<nav_msgs::OccupancyGrid>("projected_map", 5);
+  travPub = nh.advertise<nav_msgs::OccupancyGrid>("trav_map", 5);
   ros::Subscriber grid_map_sub = nh.subscribe(topic, 10, gridmap_callback);
   ros::spin();
 
